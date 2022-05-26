455fcee73ef406bda3d408602add4e26c66f4fcb
Alex Feller <alex.feller26@gmail.com>
2022-05-26 01:53:40 -0400

    Issue: Render the cards in a better way
    Ref: changed api function to render cards using custom hook, Mod: scss grid for button and card containers to autofill page in a more responsive manner

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,11 +4,9 @@ import Button from '../Button/Button';
 import Cards from '../Cards/Cards';
 
 function App() {
-  // set state for card layout
-  const [buttonState, handleButtonState] = React.useState(true);
-  const [cardState, handleCardState] = React.useState();
+  // set initial state for card layout from cocktail api
+  const [cardState, handleCardState] = React.useState([]);
 
-  // set startup state for card arrangement
   React.useEffect(() => {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`)
       .then((res) => res.json())
@@ -21,31 +19,32 @@ function App() {
       );
   }, []);
 
-  function HandleButtonFilter(event) {
+  // reusable api call on category button click
+  function useHandleButtonFilter(event) {
     const buttonValue = event.target.innerText;
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${buttonValue}`)
       .then((res) => res.json())
       .then((data) =>
         handleCardState(
           data.drinks.map((data) => {
-            return <Cards data={data} clicked={buttonState} />;
+            return <Cards data={data} />;
           })
         )
       );
   }
+
+  // loop and display all buttons
+  const buttonStuff = ['Tequila', 'Rum', 'Whiskey', 'Vodka', 'Bourbon', 'Beer', 'Wine', 'Gin'];
+  const buttonsDisplay = buttonStuff.map((element) => {
+    return <Button value={element} handleButton={useHandleButtonFilter} />;
+  });
 
   return (
     <div className="content--container">
       <h1 className="content--headline">Find Yaself a Drink</h1>
 
       {/* hold search buttons */}
-      <div className="search--container">
-        <Button value="Tequila" clicked={buttonState} handleButton={HandleButtonFilter} />
-        <Button value="Rum" clicked={buttonState} handleButton={HandleButtonFilter} />
-        <Button value="Whiskey" clicked={buttonState} handleButton={HandleButtonFilter} />
-        <Button value="Vodka" clicked={buttonState} handleButton={HandleButtonFilter} />
-        <Button value="Bourbon" clicked={buttonState} handleButton={HandleButtonFilter} />
-      </div>
+      <div className="search--container">{buttonsDisplay}</div>
 
       {/* hold cards */}
       <div className="cards--container">{cardState}</div>
