37d72fabfd84cd434c0b2e014fc799862535bbfe
Alex Feller <alex.feller26@gmail.com>
2022-06-03 15:57:40 -0400

    Mod: moved to different folder to relates more

---
+++ src/components/Button/ButtonCategories.js
@@ -0,0 +1,54 @@
+let ButtonCategories = [
+  {
+    id: 1,
+    name: 'Rum',
+    on: true,
+  },
+  {
+    id: 2,
+    name: 'Gin',
+    on: false,
+  },
+  {
+    id: 3,
+    name: 'Tequila',
+    on: false,
+  },
+  {
+    id: 4,
+    name: 'Bourbon',
+    on: false,
+  },
+  {
+    id: 5,
+    name: 'Vodka',
+    on: false,
+  },
+  {
+    id: 6,
+    name: 'Scotch',
+    on: false,
+  },
+  {
+    id: 7,
+    name: 'Brandy',
+    on: false,
+  },
+  {
+    id: 8,
+    name: 'Whiskey',
+    on: false,
+  },
+  {
+    id: 9,
+    name: 'Liqueur',
+    on: false,
+  },
+  {
+    id: 10,
+    name: 'Wine',
+    on: false,
+  },
+];
+
+export default ButtonCategories;
