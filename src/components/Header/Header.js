8c649b25e6350ce84126d3311559c98cb8a9eeb5
Alex Feller <alex.feller26@gmail.com>
2022-06-03 15:56:37 -0400

    Mod: switched navigation function to Link to

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import './Header.scss';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Header() {
-  let navigate = useNavigate();
-
   return (
     <nav className="header">
       <div className="header--content">
-        <div
-          className="headline--container"
-          onClick={() => {
-            navigate(`/`);
-          }}
-        >
+        <Link className="headline--container" to="/">
           <h1>Simple Drink Finder</h1>
-        </div>
+        </Link>
       </div>
     </nav>
   );
