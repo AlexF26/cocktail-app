eec4d0144f0f8d041f1b66a2f30886dc03914732
Alex Feller <alex.feller26@gmail.com>
2022-06-03 15:55:31 -0400

    Ref: fetch call to cocktail api and state modification

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,46 +1,43 @@
 import React from 'react';
 import Button from '../../components/Button/Button';
 import Cards from '../../components/Cards/Cards';
-import ButtonCategories from '../../App/ButtonCategories';
+import ButtonCategories from '../../components/Button/ButtonCategories';
 import './Home.scss';
 import Drinkofday from '../../components/Drink_of_Day/Drinkofday';
 
 function Home() {
-  // set initial state for card layout/button values/headline name
+  // set states
   const [cardState, handleCardState] = React.useState([]);
   const [buttonState, handleButtonState] = React.useState(ButtonCategories);
-  const [h2Name, seth2Name] = React.useState(ButtonCategories[0].name);
+  const [buttonValue, setButtonValue] = React.useState(ButtonCategories[0].name);
 
-  // card layout state init
-  React.useEffect(() => {
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${ButtonCategories[0].name}`)
-      .then((res) => res.json())
-      .then((data) => handleCardState(generateAllCards(data)));
-  }, []);
-
-  // card generating callback
+  // map through data and display card components
   function generateAllCards(data) {
     return data.drinks.map((data) => {
       return <Cards data={data} key={data.idDrink} />;
     });
   }
 
-  // gereate button color state and card layout
+  // generate button color state and card layout
   function useHandleButtonClick(id, value) {
-    seth2Name(value);
+    // switch button value to true if clicked and display blue background color
     handleButtonState(
       buttonState.map((element) => {
         return element.id === id ? { ...element, on: true } : { ...element, on: false };
       })
     );
 
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
-      .then((res) => res.json())
-      .then((data) => handleCardState(generateAllCards(data)));
+    setButtonValue(value); // buttonValue state change to button category name clicked
   }
 
-  // loop and display all buttons
-  const buttonsDisplay = buttonState.map((element) => {
+  React.useEffect(() => {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${buttonValue}`)
+      .then((res) => res.json())
+      .then((data) => handleCardState(generateAllCards(data)));
+  }, [buttonValue]);
+
+  // loop through ButtonCategories file and display button components
+  const buttonMap = buttonState.map((element) => {
     return (
       <Button
         key={element.id}
@@ -58,14 +55,14 @@ function Home() {
         <div className="hero--content">
           <h1 className="content--headline">Find Yaself a Drink</h1>
           {/*Category buttons */}
-          <div className="search--container">{buttonsDisplay}</div>
+          <div className="search--container">{buttonMap}</div>
         </div>
       </div>
 
       <div className="content--container">
         {/* Card Layout */}
         <section>
-          <h2 className="section--headline">Find a drink with {h2Name}</h2>
+          <h2 className="section--headline">Find a drink with {buttonValue}</h2>
           <div className="cards--container">{cardState} </div>
         </section>
         <section>
